0500f10b2460870774051e2b1a282a536f3a77f4
swapnil5775 <swapnil5775@users.noreply.github.com>
2025-02-25 18:41:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import os
 from flask import Flask
 from institution_list import institution_bp
 from research import research_bp
-from seasonality import seasonality_bp, seasonality_etf_bp  # Add seasonality_etf_bp
+from seasonality import seasonality_bp
 from etf_research import etf_research_bp
 
 app = Flask(__name__)
 
+# Register blueprints
 app.register_blueprint(institution_bp)
 app.register_blueprint(research_bp)
 app.register_blueprint(seasonality_bp)
-app.register_blueprint(seasonality_etf_bp)  # Register here
 app.register_blueprint(etf_research_bp)
 
 if __name__ == "__main__":
